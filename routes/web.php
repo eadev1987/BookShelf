85751cdc93be23d83caa6e1458dec26f83c11169
eadev1987 <edgars.augustans@gmail.com>
2025-02-18 13:52:23 +0200

    Updated books list

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [BooksController::class, 'index']);
